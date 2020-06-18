4194a68faa48d86bb61e49fa622884826fa86aa7
lgduke <66661195+lgduke@users.noreply.github.com>
2020-06-18 20:26:08 +0900

    life coding nodejs 36 after

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-function templateHTML(title,list,body){
+function templateHTML(title,list,body,control){
   return `<!doctype html>
   <html>
   <head>
@@ -13,7 +13,7 @@ function templateHTML(title,list,body){
   <body>
     <h1><a href="/">WEB22</a></h1>
     ${list}
-    <a href="/create">create</a>
+    ${control}
     ${body}
   </body>
   </html>`;
@@ -50,7 +50,8 @@ var app = http.createServer(function(request,response){
           var list = templateList(filelist);
 
           var body = `<h2>${title}</h2>${desc}`;
-          var template = templateHTML(title,list,body);
+          var control =`<a href="/create">create</a>`;
+          var template = templateHTML(title,list,body,control);
             response.writeHead(200);
             response.end(template);
 
@@ -69,14 +70,16 @@ var app = http.createServer(function(request,response){
 //            console.log(err);
             var list = templateList(filelist);
             var body = `<h2>${title}</h2>${desc}`;
-            var template = templateHTML(title,list,body);
+            var control =`<a href="/create">create</a>
+                <a href="/update?id=${title}">update</a>`;
+            var template = templateHTML(title,list,body,control);
             response.writeHead(200);
             response.end(template);
             //      response.end(queryData.id);
             //      response.end('Duke : ' + url);
             //        console.log(queryData.id);
           });
-          });
+        });
       }
     } else if(pathname === '/create') {
       var testFolder = './data/';
@@ -90,7 +93,7 @@ var app = http.createServer(function(request,response){
         console.log("queryData is ", queryData)
         console.log("pathname is ", pathname)
 
-        var body = `<form action="http://localhost:3000/create_process", method="post">
+        var body = `<form action="/create_process", method="post">
           <p><input type="text" name="title" placeholder="title"></p>
           <p>
             <textarea name="desc" rows="8" cols="80" placeholder="description" ></textarea>
@@ -99,7 +102,7 @@ var app = http.createServer(function(request,response){
             <input type="submit">
           </p>
         </form>`;
-        var template = templateHTML(title,list,body);
+        var template = templateHTML(title,list,body,'');
           response.writeHead(200);
           response.end(template);
     });
@@ -118,6 +121,62 @@ var app = http.createServer(function(request,response){
       });
       console.log(post.title);
       console.log(post.desc);
+    });
+
+  } else if(pathname === '/update') {
+        var testFolder = './data/';
+        console.log(pathname);
+        fs.readdir(testFolder, function(error, filelist){
+          console.log(filelist);
+    //          var title = "Welcome";
+          var desc = "Hello. Node.Js";
+
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, desc) {
+    //            console.log(desc);
+    //            console.log(err);
+            var list = templateList(filelist);
+            var body = `<form action="/update_process", method="post">
+              <input type="hidden" name="id" value="${title}">
+              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <p>
+                <textarea name="desc" rows="8" cols="80" placeholder="description">${desc}</textarea>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>`;
+            var control =`<a href="/create">create</a>
+                <a href="/update?id=${title}">update</a>`;
+            var template = templateHTML(title,list,body,control);
+            response.writeHead(200);
+            response.end(template);
+            //      response.end(queryData.id);
+            //      response.end('Duke : ' + url);
+            //        console.log(queryData.id);
+          });
+        });
+  } else if(pathname === '/update_process'){
+    var body ='';
+    request.on('data', function(data){
+      body = body + data
+    });
+    request.on('end',function() {
+      var post = qs.parse(body);
+      var id = post.id;
+      var title = post.title;
+      var desc = post.desc;
+      fs.rename(`data/${id}`,`data/${title}`,function(error){
+        fs.writeFile(`data/${title}`, desc, 'utf8', function(err){
+          response.writeHead(302, {Location:`/?id=${title}` });
+          response.end();
+        });
+      });
+      console.log(post);
+      /*
+
+      */
+//      console.log(post.title);
+//      console.log(post.desc);
     });
 
   } else {
